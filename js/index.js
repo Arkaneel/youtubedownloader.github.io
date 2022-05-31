e4994eaac74edcd21f9b829d1b11f8c397a4c6c2
Arkaneel <97721855+Arkaneel@users.noreply.github.com>
2022-05-31 20:45:04 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -7,3 +7,5 @@
   function downloadVideo(url,res) {
       $('.download-video').html('<iframe style="width:200px;height:60px;border:0;overflow:hidden;" scrolling="no" src="https://loader.to/api/button/?url='+url+'&f='+res+'"></iframe>');
   }
+
+console.log("api fetched from node//Arkaneel//loader successfully");

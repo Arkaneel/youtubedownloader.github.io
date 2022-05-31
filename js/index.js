345af625d1e91463cc4d3ec37c20d287ac80451a
Arkaneel <arkaneelroy2007@gmail.com>
2022-05-31 17:47:10 +0530

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+ $(".download").click(function(){
+      var url = $(".url-entry").val();
+      var res = $(".format").children("option:selected").val();
+    var src =""+url+"="+res+"";
+    downloadVideo(url,res);
+  });
+  function downloadVideo(url,res) {
+      $('.download-video').html('<iframe style="width:200px;height:60px;border:0;overflow:hidden;" scrolling="no" src="https://loader.to/api/button/?url='+url+'&f='+res+'"></iframe>');
+  }
